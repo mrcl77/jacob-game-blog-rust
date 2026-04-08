fde690382fab3054f95e84b7b993fd37097319a7
Kanewersa <kanewers@gmail.com>
2026-04-08 23:04:29 +0200

    Change navbar active item on click

--- static/bundle.js
+++ static/bundle.js
@@ -821,4 +821,16 @@
     containers: ["#swup", "#main-nav"],
     plugins: [new l2()]
   });
+  document.addEventListener("click", (e2) => {
+    const link = e2.target.closest("#main-nav a");
+    if (link && link.href) {
+      const navLinks = document.querySelectorAll("#main-nav a");
+      navLinks.forEach((navLink) => {
+        navLink.classList.remove("text-[#4dc9f1]", "border-b-4", "border-[#4dc9f1]", "pb-1");
+        navLink.classList.add("text-[#76757a]", "hover:text-[#4dc9f1]");
+      });
+      link.classList.remove("text-[#76757a]", "hover:text-[#4dc9f1]");
+      link.classList.add("text-[#4dc9f1]", "border-b-4", "border-[#4dc9f1]", "pb-1");
+    }
+  });
 })();
